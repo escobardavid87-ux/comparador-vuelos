fe09599dd43a2ad41287a134f5075761b79aa12f
escobardavid87-ux <escobar.david87@gmail.com>
2026-09-19 11:34:28 +0200

    Create AvisoAfiliados.js

---
+++ components/AvisoAfiliados.js
@@ -0,0 +1,7 @@
+export default function AvisoAfiliados() {
+  return (
+    <p style={{ fontSize: 12, color: '#777', marginTop: 24, textAlign: 'center' }}>
+      Somos un comparador de vuelos. Si reservas a través de nuestros enlaces, podemos recibir una comisión sin coste adicional para ti.
+    </p>
+  );
+}
