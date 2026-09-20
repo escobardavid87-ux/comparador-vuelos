dc9da370872fd839dfdae85645da411f2a72fa52
escobardavid87-ux <escobar.david87@gmail.com>
2026-09-20 21:54:32 +0200

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="es">
+      <Head>
+        <script
+          data-cfasync="false"
+          data-no-defer="1"
+          dangerouslySetInnerHTML={{
+            __html: `(function () {
+  var script = document.createElement("script");
+  script.async = 1;
+  script.setAttribute("data-cmp-ab","2");
+  script.src = "https://emrldco.com/NTc1MTE3.js?t=575117";
+  document.head.appendChild(script);
+})();`,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
