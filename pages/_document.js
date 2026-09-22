d4a1c605fc94ca75283d12dbe10b8b167ebc6e2a
escobardavid87-ux <escobar.david87@gmail.com>
2026-09-22 21:34:45 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500;9..144,600;9..144,700&family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <script
           data-cfasync="false"
           data-no-defer="1"
@@ -29,4 +29,4 @@ export default function Document() {
       </body>
     </Html>
   );
-            }
+}
