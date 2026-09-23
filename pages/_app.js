a266b611cab43120a45afd6481634b6f5a98c62e
escobardavid87-ux <escobar.david87@gmail.com>
2026-09-23 07:55:08 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <div key={router.asPath} className="page-transition">
+      <Component {...pageProps} />
+    </div>
+  );
 }
